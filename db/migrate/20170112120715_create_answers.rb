dc7e92f395383e7fce978e4b79d329581dd7c59c
groovestation31785 <email": null>
2017-01-12 12:32:02 -0800

    Create migration for answers table

---
+++ db/migrate/20170112120715_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.integer :author_id, { null: false }
+      t.text :content
+      t.integer :vote_count
+
+      t.timestamps null: false
+    end
+  end
+end
