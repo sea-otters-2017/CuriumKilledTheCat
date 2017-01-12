c1116978d309e40e88d45038c0753b17587c044a
Austin Kading <austinekading@gmail.com>
2017-01-12 12:10:11 -0800

    Update 20170112111046_create_comments.rb

--- db/migrate/20170112111046_create_comments.rb
+++ db/migrate/20170112111046_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :comments
+      t.text :content
       t.integer :author_id, {null: false}
       t.integer :vote_count
       t.index :author_id
