f6bca596fa761ea45e813a73733042401356f210
groovestation31785 <email": null>
2017-01-12 12:51:40 -0800

    Write tests for answer associations and validations

--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -2,28 +2,38 @@ require 'spec_helper'
 
 describe Answer do
   before(:each) do
-    @answer = Answer.new(author_id: 1, post_id: 1)
+    @answer = Answer.new(author_id: 1, content: "Here is my content!", vote_count: 3456)
   end
 
   describe 'associations' do
     it 'verifies the belongs_to association of user' do
-      should belong_to(:user)
+      should belong_to(:author)
+    end
+
+    it 'verifies the has_many association of votes' do
+      should have_many(:votes)
     end
   end
 
   describe 'attributes' do
     it "returns the id of the vote's author" do
-      expect(@vote.author_id).to eq(1)
+      expect(@answer.author_id).to eq(1)
     end
 
     it "returns the id of the vote's post" do
-      expect(@vote.post_id).to eq(1)
+      expect(@answer.vote_count).to eq(3456)
     end
   end
 
   describe 'validations' do
     it 'validates that the vote has an author_id' do
       should validate_presence_of(:author_id)
+    end
+    it 'validates that the vote has content' do
+      should validate_presence_of(:content)
+    end
+    it 'validates that the vote has a vote_count' do
+      should validate_presence_of(:vote_count)
     end
   end
 end
