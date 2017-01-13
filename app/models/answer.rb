14693f6b9135df53a795e9664ba29488947f176c
groovestation31785 <email": null>
2017-01-13 11:12:32 -0800

    Update associations in answer, question, answer, and user

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
+  has_many :comments
   has_many :votes, class_name: 'AnswerVote'
 
-  validates :author_id, :content, :vote_count, presence: true
+  validates :author_id, :content, :question_id, :vote_count, presence: true
 end
