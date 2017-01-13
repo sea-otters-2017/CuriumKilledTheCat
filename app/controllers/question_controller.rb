fcd8a68fc1e15d14790fb4ee204901bc1213aa14
Katherine Guenther <guenther@peak.org>
2017-01-13 12:40:43 -0800

    Add buttons for new answer and new comment

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -5,7 +5,7 @@ end
 
 # Belongs in vote controller?.
 # Just checking that params[:vote_count] = 1 or -1
-post '/questions/:id/votes' do 
+post '/questions/:id/votes' do
   p "*******************"
   p params
   p "*******************"
