6715d1290b5113a0b343d7b4f31ecb09b45224e0
Katherine Guenther <guenther@peak.org>
2017-01-13 15:25:39 -0800

    Add answers and comments to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ Answer.delete_all
 Comment.delete_all
 
 def generate_id(exclude_id)
-  valid_ids = [1..10].select { |number| number != exclude_id }
-  valid_ids.select
+  ids = Array(1..10).select { |number| number != exclude_id }
+  ids.sample
 end
 
 10.times do
@@ -17,7 +17,7 @@ end
     5.times do
       answer = Answer.create(author_id: generate_id(user.id), content: Faker::Hipster.paragraph, question_id: question.id, vote_count: 0)
       3.times do
-        comment = Comment.new(content: Faker::Hipster.sentence, author_id: generate_id(user.id), answer_id: answer.id, vote_count: 0)
+        comment = Comment.create(content: Faker::Hipster.sentence, author_id: generate_id(user.id), answer_id: answer.id, vote_count: 0)
       end
     end
   end
