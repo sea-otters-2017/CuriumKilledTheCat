2706d58d03c3669994ca0f53f0eb0e5a93fd21e0
Austin Kading & Katherine Guenther <pair+austin-kading.katherineguenther@devbootcamp.com>
2017-01-13 12:49:23 -0800

    Add routes for getting the answer form page and posting the answer

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -2,3 +2,14 @@ get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'question/show'
 end
+
+# goes to the form page for answering a question
+get '/questions/:id/answers/new' do
+  @question = Question.find(params[:id])
+  erb :'answer/new'
+end
+
+post '/questions/:id/answers' do
+  @question = Question.find(params[:id])
+  @answer = Answer.new(params[:answer])
+end
