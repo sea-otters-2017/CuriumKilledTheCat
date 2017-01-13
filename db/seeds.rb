7aa40eced7c698a7f0d43ef800be3792b15e2547
Katherine Guenther <guenther@peak.org>
2017-01-13 14:50:41 -0800

    Add answers and comments to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,23 @@ require 'faker'
 
 User.delete_all
 Question.delete_all
+Answer.delete_all
+Comment.delete_all
+
+def generate_id(exclude_id)
+  valid_ids = [1..10].select { |number| number != exclude_id }
+  valid_ids.select
+end
 
 10.times do
   user = User.create(username: Faker::Internet.user_name, password: 'pw')
   15.times do
-    question = Question.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, author_id: user.id)
+    question = Question.create(title: Faker::Hacker.say_something_smart, content: Faker::ChuckNorris.fact, author_id: user.id)
+    5.times do
+      answer = Answer.create(author_id: generate_id(user.id), content: Faker::Hipster.paragraph, question_id: question.id, vote_count: 0)
+      3.times do
+        comment = Comment.new(content: Faker::Hipster.sentence, author_id: generate_id(user.id), answer_id: answer.id, vote_count: 0)
+      end
+    end
   end
 end
