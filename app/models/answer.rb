1cafc7675f88bd787075fe633dbd2fc9b75f2d22
Ian Peter Greenburg <iangreenburg@yahoo.com>
2017-01-13 11:17:18 -0800

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   has_many :comments
+  belongs_to :question
   has_many :votes, class_name: 'AnswerVote'
 
   validates :author_id, :content, :question_id, :vote_count, presence: true
