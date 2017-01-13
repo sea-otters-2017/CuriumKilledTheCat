bf738aab3168db399833de04f2a0d525672527da
Ian Peter Greenburg <email": null>
2017-01-13 12:32:35 -0800

    adding new question form

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,3 +1,25 @@
+get '/questions/new' do
+  unless User.find_by_id(session[:user_id]).try(:id)
+    return erb :'404'
+  end
+
+  @question = Question.new
+  erb :'/question/new'
+end
+
+post '/questions' do
+  params[:question][:author_id] = User.find_by_id(session[:user_id]).id
+  unless params[:question][:author_id]
+    return erb :'404'
+  end
+  @question = Question.create(params[:question])
+  if @question.persisted?
+    redirect ('/questions/' + @question.id.to_s)
+  else
+    erb :'/question/new'
+  end
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'question/show'
