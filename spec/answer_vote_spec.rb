fe78edae537cad194e3ec4454f00ec58823c71b0
Ian Peter Greenburg <email": null>
2017-01-13 15:49:52 -0800

    adding validation so that a user can't vote more than once

--- spec/answer_vote_spec.rb
+++ spec/answer_vote_spec.rb
@@ -25,6 +25,8 @@ describe AnswerVote do
     end
 
     describe 'validations' do
+
+
       it 'validates that the answer has an author_id' do
         should validate_presence_of(:author_id)
       end
@@ -40,8 +42,8 @@ describe AnswerVote do
         should validate_inclusion_of(:vote_count).in_array([-1,1])
       end
 
+      subject { AnswerVote.new(answer_id: 1, vote_count: 1) }
       it 'validates that author can\'t upvote an answer more than once' do
-        subject { AnswerVote.new(author_id: 1, answer_id: 1, vote_count: 1) }
         should validate_uniqueness_of(:author_id).scoped_to(:answer_id)
       end
     end
