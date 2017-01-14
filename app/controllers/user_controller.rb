cc51fc697e7702a76a289cdf3f1d0e7d9b7cc3d9
groovestation31785 <email": null>
2017-01-14 13:52:57 -0800

    Update routes to support the AJAX errors

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,7 +8,7 @@ end
 post '/user' do
   @user = User.create(params[:user])
   if @user.persisted?
-    session[:user_id] = @user.id
+    session_login(@user.id)
     redirect '/'
   else
     erb :'user/new'
@@ -23,7 +23,7 @@ end
 post '/user/login' do
   @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
   if @user
-    session[:user_id] = @user.id
+    session_login(@user.id)
     redirect '/'
   else
     erb :'/user/login'
@@ -38,7 +38,7 @@ end
 get '/user/:id' do
   @user = User.find_by_id(params[:id])
 
-  if @user && @user == User.find_by_id(session[:user_id])
+  if @user && @user == User.find_by_id(session_user_id)
     erb :'user/personal_profile'
   elsif @user
     erb :'user/profile'
