621d5657d16e0d64d89b29e0810b66db7b1af75d
Austin Kading & Katherine Guenther <pair+austin-kading.katherineguenther@devbootcamp.com>
2017-01-13 17:44:38 -0800

    Create spec test for creating a new answer

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -1,17 +1,42 @@
 require 'spec_helper'
 
 describe 'Answer Controller' do
-  describe 'GET "/questions/:id/answers/new"' do
-    before :all do
-      @question = Question.create(title: "How do I computer?", content: "Computering is hard. How do I do it?", author_id: 1)
-    end
+  before :each do
+    User.delete_all
+    Answer.delete_all
+    Question.delete_all
+    @user = User.create(username: 'new-user', password: 'password')
+    @question = Question.create(title: "How do I computer?", content: "Computering is hard. How do I do it?", author_id: @user.id)
+    @answer = Answer.create(content: "I don't know", question_id: @question.id, author_id: @user.id)
 
+  end
+
+  describe 'GET "/questions/:id/answers/new"' do
     it 'loads the new answer page' do
-      get("/questions/#{@question.id}/answers/new")
+      get "/questions/#{@question.id}/answers/new"
       expect(last_response).to be_ok
     end
   end
 
-  describe
+  describe "POST 'questions/:id/answers'" do
+    it 'creates a new answer' do
+      expect {
+        post(
+          "/questions/#{@question.id}/answers",
+          { content: 'some content' },
+          'rack.session' => { :user_id => @user.id }
+        )
+      }.to change{Answer.count}.by 1
+
+    end
+
+    it 'redirects to the question' do
+      #
+      # expect(last_response).to be_redirect
+      #
+      # follow_redirect!
+
+    end
+  end
 
 end
