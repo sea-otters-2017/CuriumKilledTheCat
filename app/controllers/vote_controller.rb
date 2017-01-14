c1f29be6d7c35933ba88a13ab9fb866b66ad607c
Ian Peter Greenburg <email": null>
2017-01-13 16:42:54 -0800

    when users vote on a comment they can change or undo their vote

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -2,13 +2,26 @@
 post '/questions/:question_id/votes' do
   @author = User.find_by_id(session[:user_id])
   @question = Question.find_by_id(params[:question_id])
+
   unless @author && @question
     return erb :'404'
   end
+
+  if @author.voted_questions.include? @question
+    vote = QuestionVote.find_by(author_id: @author.try(:id), question_id: @question.try(:id))
+    if vote.vote_count == params[:vote_count].to_i
+      vote.destroy
+    else
+      vote.update(:vote_count => params[:vote_count])
+    end
+    redirect '/questions/' + @question.id.to_s
+  end
+
   @vote = QuestionVote.new(author_id: @author.try(:id), question_id: @question.try(:id), vote_count: params[:vote_count])
+
   if @vote.valid?
     @vote.save
   end
-  redirect '/questions/' + @question.id.to_s
 
+  redirect '/questions/' + @question.id.to_s
 end
