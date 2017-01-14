e8382126097f07fde839ba4a67f03f3b58085ab4
Ian Peter Greenburg <email": null>
2017-01-13 16:33:54 -0800

    correcting specs to test for current program state

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :author_id
   has_many :questions, foreign_key: :author_id
 
+
+  has_many :voted_questions, through: :question_votes, source: :question
+  has_many :voted_answers, through: :answer_votes, source: :answer
+  has_many :voted_comments, through: :comment_votes, source: :comment
+
   validates :username, presence: true, uniqueness: true
   has_secure_password
 end
