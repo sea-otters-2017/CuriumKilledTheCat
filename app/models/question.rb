5e800e7a46f19b88a1d2bdef92570ca88950b73b
Katherine Guenther <guenther@peak.org>
2017-01-13 18:37:34 -0800

    Add question and comments display

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
 
   has_many :question_votes
   has_many :answers
-  has_many :comments#, through: :answers
+  has_many :comments, through: :answers
 
   validates :title, :author_id, :content, presence: true
 end
