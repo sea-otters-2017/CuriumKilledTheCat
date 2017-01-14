242fee93af770d163e3710ef090c313c93ed095f
Katherine Guenther <guenther@peak.org>
2017-01-13 23:11:55 -0800

    Add ajax post request with vote data

--- public/js/vote.js
+++ public/js/vote.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
     event.preventDefault();
     alert('Listening on vote-button-container');
     var url = $(event.target).closest('form').attr('action');
+    var hidden_field = $(event.target).find("input[name='vote-count']");
+    var data = { vote_count: hidden_field.val() };
+    console.log(data);
+
     $.ajax({
       type: 'POST',
       url: url,
-      data: "{vote_count: 1}",
+      data: data,
       dataType: "text",
       success: function(resultData){
           alert("Success");
