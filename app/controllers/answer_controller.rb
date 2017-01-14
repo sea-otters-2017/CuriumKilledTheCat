022d61a7b0eba01ad50115f2f33f58cd01857d15
Austin Kading & Katherine Guenther <pair+austin-kading.katherineguenther@devbootcamp.com>
2017-01-13 20:45:04 -0800

    Update answer controller to support AJAX functionality

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,3 +1,5 @@
+require 'sinatra/json'
+
 
 # goes to the form page for answering a question
 get '/questions/:id/answers/new' do
@@ -15,12 +17,20 @@ post '/questions/:id/answers' do
   end
   @question = Question.find(params[:id])
   @answer = Answer.create(content: params[:content], author_id: session_user_id, question_id: @question.id)
-  redirect "/questions/#{@question.id}"
+  # hash = {content: , author_id: }
+
+  if request.xhr?
+    hash = {content: @answer.content, author_id: @answer.author_id }
+    content_type :json
+    JSON.generate(hash)
+  else
+    redirect "/questions/#{@question.id}"
+  end
 end
 
 get '/new_answer/:question_id' do
   if request.xhr?
     @question = Question.find_by_id(params[:question_id])
-    erb :'partials/_new_answer'
+    erb :'partials/_new_answer', layout: false
   end
 end
