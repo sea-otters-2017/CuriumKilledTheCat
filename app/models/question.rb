7c4a962bb259e6733b6e3bd22e87bc7b68d76042
Katherine Guenther <guenther@peak.org>
2017-01-13 17:11:59 -0800

    Fix association between question and comment

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
 
   has_many :question_votes
-  has_many :comments
   has_many :answers
-
+  has_many :comments#, through: :answers
 
   validates :title, :author_id, :content, presence: true
 end
