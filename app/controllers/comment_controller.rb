79421229b436ec5c023d88f2519b64de74de0ad7
Ian Peter Greenburg <email": null>
2017-01-14 16:55:27 -0800

    ajax for new comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -11,5 +11,24 @@ post '/answers/:id/new' do
 
   @comment = Comment.create(author_id: session_user_id, content: params[:content], answer_id: params[:id].to_i)
   question = Answer.find_by_id(params[:id]).question
-  redirect '/questions/' + question.id.to_s
+
+
+  if request.xhr?
+    hash = {content: @comment.content, author_id: @comment.author_id}
+    json hash
+  else
+    redirect '/questions/' + question.id.to_s
+  end
+end
+
+get '/new_comment/:answer_id' do
+  unless session_user
+    status 404
+    return erb :'404', layout: false
+  end
+
+  if request.xhr?
+    @answer = Answer.find_by_id(params[:answer_id])
+    erb :'partials/_new_comment', layout: false
+  end
 end
