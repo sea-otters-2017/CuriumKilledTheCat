f1f2be36dde7ec8e21d0e5509fa1220289bdb89a
Ian Peter Greenburg <email": null>
2017-01-14 20:50:10 -0800

    adding abililty to vote for comments and answers

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -26,41 +26,10 @@ post '/questions/:question_id/votes' do
   redirect '/questions/' + @question.id.to_s
 end
 
-
-
-post '/comments/:comment_id/votes' do
-  @author = User.find_by_id(session[:user_id])
-  @comment = comment.find_by_id(params[:comment_id])
-
-  unless @author && @comment
-    return erb :'404'
-  end
-
-  if @author.voted_comments.include? @comment
-    vote = CommentVote.find_by(author_id: @author.try(:id), comment_id: @comment.try(:id))
-    if vote.vote_count == params[:vote_count].to_i
-      vote.destroy
-    else
-      vote.update(:vote_count => params[:vote_count])
-    end
-    redirect '/comments/' + @comment.id.to_s
-  end
-
-  @vote = CommentVote.new(author_id: @author.try(:id), comment_id: @comment.try(:id), vote_count: params[:vote_count])
-
-  if @vote.valid?
-    @vote.save
-  end
-
-  redirect '/comments/' + @question.id.to_s
-end
-
-
-
-
 post '/answers/:answer_id/votes' do
-  @author = User.find_by_id(session[:user_id])
-  @answer = answer.find_by_id(params[:answer_id])
+  @author = session_user
+  @answer = Answer.find_by_id(params[:answer_id])
+  @question = @answer.try(:question)
 
   unless @author && @answer
     return erb :'404'
@@ -73,10 +42,39 @@ post '/answers/:answer_id/votes' do
     else
       vote.update(:vote_count => params[:vote_count])
     end
-    redirect '/answers/' + @answer.id.to_s
+    redirect '/questions/' + @question.id.to_s
   end
 
   @vote = AnswerVote.new(author_id: @author.try(:id), answer_id: @answer.try(:id), vote_count: params[:vote_count])
+
+  if @vote.valid?
+    @vote.save
+  end
+
+  redirect '/questions/' + @question.id.to_s
+end
+
+
+post '/comments/:comment_id/votes' do
+  @author = User.find_by_id(session[:user_id])
+  @comment = Comment.find_by_id(params[:comment_id])
+  @question = @comment.try(:answer).try(:question)
+
+  unless @author && @comment
+    return erb :'404'
+  end
+
+  if @author.voted_comments.include? @comment
+    vote = CommentVote.find_by(author_id: @author.try(:id), comment_id: @comment.try(:id))
+    if vote.vote_count == params[:vote_count].to_i
+      vote.destroy
+    else
+      vote.update(:vote_count => params[:vote_count])
+    end
+    redirect '/questions/' + @question.id.to_s
+  end
+
+  @vote = CommentVote.new(author_id: @author.try(:id), comment_id: @comment.try(:id), vote_count: params[:vote_count])
 
   if @vote.valid?
     @vote.save
