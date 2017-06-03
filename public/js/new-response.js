829c2dd8250a66b014fe99964074c7243162b83b
Ian Peter Greenburg <email": null>
2017-06-02 18:03:29 -0700

    styling

--- public/js/new-response.js
+++ public/js/new-response.js
@@ -3,20 +3,15 @@ $(document).ready(function() {
 /// new answer listener
     $("#event-listener").on('submit', '#new-answer-button', function(event) {
       event.preventDefault();
-
-      var questionId = ($(this).children().prop('id'))
-      $this = $(this)
       resetPage();
-
+      $this = $(this)
       $.ajax({
         type: 'GET',
-        url: '/new_answer/' + questionId
-
+        url: $this.prop('action')
       }).done(function(data){
         $this.addClass('hidden');
         $("#ajax-answer").append(data);
         $('#new-answer-form').children('textarea').focus();
-
       }).error(function(response) {
         alert('You must be logged in to leave an answer')
       })
@@ -24,45 +19,32 @@ $(document).ready(function() {
 
     $("#event-listener").on('submit', '#new-answer-form', function(event) {
       event.preventDefault();
-      var data = $(this).serialize();
-      var questionId = ($(this).children('div').children('.submit-answer').prop('id'));
-
+      var $this = $(this)
       $.ajax({
         type: 'POST',
-        url:'/questions/'+ questionId + '/answers',
-        data: data
-
+        url: $this.prop('action'),
+        data: $this.serialize()
       }).done(function(data){
-
         $('#answers-list').append(data);
         resetPage();
-
       }).error(function(data){
         alert('answer field cannot be left blank')
       })
     });
 
-    $('#event-listener').on('click', '.cancel', function(event){
-      event.preventDefault();
-      resetPage();
-    })
-
 ///new comment listener
     $("#event-listener").on('click', '#new-comment-button', function(event) {
       event.preventDefault();
       resetPage();
-      var answerId = ($(this).closest('div').prop('id'))
       var $this = $(this)
-
+      var answerId = $this.closest('div').prop('id')
       $.ajax({
         type: 'GET',
-        url: '/new_comment/' + answerId
-
+        url: $this.closest('form').prop('action')
       }).done(function(data){
         $this.parent().addClass('hidden');
-        $("#".concat(answerId)).append(data);
+        $("#" + answerId).append(data);
         $('#new-comment-form').children('textarea').focus();
-
       }).error(function(response) {
         alert('You must be logged in to leave a comment')
       })
@@ -70,22 +52,24 @@ $(document).ready(function() {
 
     $("#event-listener").on('submit', '#new-comment-form', function(event) {
       event.preventDefault();
-      var data = $(this).serialize();
-      var answerId = ($(this).closest('div.new-comment-wrapper').prop('id'));
-
+      var $this = $(this)
+      var tableId = $this.closest('td').children('table').prop('id')
       $.ajax({
         type: 'POST',
-        url:'/answers/'+ answerId + '/new',
-        data: data
+        url: $this.prop('action'),
+        data: $this.serialize()
       }).done(function(data){
-
-        var table = '#comments-' + answerId
-        $(table).append(data);
+        $('#' + tableId).append(data);
         resetPage();
       }).error(function(data){
         alert('comment can\'t be left blank')
       })
     });
+
+    $('#event-listener').on('click', '.cancel', function(event){
+      event.preventDefault();
+      resetPage();
+    })
 });
 
 function resetPage() {
